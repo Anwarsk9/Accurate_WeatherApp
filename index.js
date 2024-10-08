478862caa3eb21cf275a0c028371f413dc022b96
Anwarsk9 <anwarsk91678@gmail.com>
2024-10-08 23:55:11 +0530

    Root path changed

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import "dotenv/config";
 import axios from "axios";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 import cors from "cors";
 
 const app = express();
@@ -9,11 +11,20 @@ const coreOptions = {
   origin: "http://127.0.0.1:5500", // Your frontend origin
   optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
 };
-
+// Simulate __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+// Serve static files from the public directory
+app.use(express.static(join(__dirname, "public")));
 app.use(cors(coreOptions));
 
-const port = 3000;
+const port = process.env.PORT;
 const api = "https://api.openweathermap.org/data/2.5/weather?";
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 app.get("/weather", async (req, res) => {
   const apiID = process.env.APIKEY;
   const city = req.query.city;
@@ -27,4 +38,6 @@ app.get("/weather", async (req, res) => {
     res.status(500).send("Error fecthing weather data");
   }
 });
-app.listen(port, () => console.log("App is listening"));
+app.listen(port, () =>
+  console.log(`Server is running on port ${process.env.PORT}`)
+);
